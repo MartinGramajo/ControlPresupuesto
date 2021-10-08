53da65e6059dccc976810ec138c63f8a9dec5b7b
magus <martingramajo08@gmail.com>
2021-10-08 16:40:28 -0300

    agrego componente gasto

---
+++ src/components/Gasto.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Gasto({gasto}) {
+    return (
+        <li className="gastos">
+            <p>
+                {gasto.nombre}
+                <span className="gasto">
+                    $ {gasto.cantidad}
+                </span>
+            </p>
+        </li>
+    )
+}
