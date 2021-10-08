d074107dd4a709598d8023036c0d027ae9b436a9
magus <martingramajo08@gmail.com>
2021-10-08 16:41:17 -0300

    agrego funcionalidad al componente y carga condicional del componente

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
-import './App.css';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Pregunta from './components/Pregunta';
+import Formulario from './components/Formulario';
+import Listado from './components/Listado';
 
 function App() {
+  //Definir el State
+  const [presupuesto, guardarPresupuesto] = useState(0);
+  const [restante, guardarRestante] = useState(0);
+  const [mostrarPregunta, actualizarPreguntar] = useState(true);
+  const [gastos, guardarGastos] = useState([]);
+
+
+  //  Funcion que se ejecuta cuando se agrega un nuevo gasto 
+  const agregarNuevoGasto = gasto => {
+    guardarGastos([
+      ...gastos, gasto
+    ])
+  }
+
   return (
     <div className="container" >
       <header>
         <h1> Gasto semanal </h1>
         <div className="contenido-principal contenido">
-          <Pregunta />
+          {mostrarPregunta ? (<Pregunta
+            guardarPresupuesto={guardarPresupuesto}
+            guardarRestante={guardarRestante}
+            actualizarPreguntar={actualizarPreguntar}
+          />) : (
+
+            <div className="row">
+              <div className="one-half column">
+                <Formulario
+                  agregarNuevoGasto={agregarNuevoGasto}
+                />
+              </div>
+              <div className="one-half column">
+                  <Listado
+                    gastos={gastos}
+                  />
+              </div>
+            </div>
+          )}
         </div>
       </header>
     </div>
--- src/components/Pregunta.jsx
+++ src/components/Pregunta.jsx
@@ -1,13 +1,15 @@
-import Button from "@restart/ui/esm/Button";
 import React, { Fragment, useState } from "react";
-import { Form } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 import Error from "./Error";
 
-const Pregunta = () => {
+const Pregunta = ({
+  guardarPresupuesto,
+  guardarRestante,
+  actualizarPreguntar,
+}) => {
   // Definir el state de cantidad
-    const [cantidad, setGuardarCantidad] = useState(0);
-    const [error, setGuardarError] = useState(false);
-    
+  const [cantidad, setGuardarCantidad] = useState(0);
+  const [error, GuardarError] = useState(false);
 
   // Funcion que lee el presupuesto
   const definirPresupuesto = (e) => {
@@ -15,24 +17,25 @@ const Pregunta = () => {
   };
 
   // Submit para definir el presupuesto
-    const agregarPresupuesto = e => {
-        e.preventDefault();
+  const agregarPresupuesto = (e) => {
+    e.preventDefault();
 
-        // Validar
-        if (cantidad < 1 || isNaN) {
-            setGuardarError(true);
-            return;
-        }
-
-        // Si se pasa la validacion 
-        setGuardarError(false)
+    // Validar
+    if (cantidad < 1 || isNaN(cantidad)) {
+      GuardarError(true);
+      return;
     }
-    
-    
+    // Si se pasa la validacion
+    GuardarError(false);
+    guardarPresupuesto(cantidad);
+    guardarRestante(cantidad);
+    actualizarPreguntar(false);
+  };
+
   return (
     <Fragment>
-          <h2>Coloca tu presupuesto</h2>
-          {error ? <Error mensaje="El Presupuesto es Incorrecto" /> : null}
+      <h2>Coloca tu presupuesto</h2>
+      {error ? <Error mensaje="El Presupuesto es Incorrecto" /> : null}
       <Form onSubmit={agregarPresupuesto}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Control
